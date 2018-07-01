8e0117fd2e4068fb2a63392dbb66083dca2cd3fe
stigsanek <stigsanek@yandex.ru>
2018-07-01 18:07:59 +0400

    Добавлена возможность переключения слайдов

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ form.addEventListener("submit", function (evt) {
   } else {
     if (isStorageSupport) {
       localStorage.setItem("nameField", nameField.value);
-      localStorage.setItem("emailField", emailField.value); //
+      localStorage.setItem("emailField", emailField.value);
     }
   }
 });
@@ -85,3 +85,81 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+var promoBtn1 = document.querySelector(".promo-btn-1");
+var promoBtn2 = document.querySelector(".promo-btn-2");
+var promoBtn3 = document.querySelector(".promo-btn-3");
+
+var promoSlide1 = document.querySelector(".promo-1");
+var promoSlide2 = document.querySelector(".promo-2");
+var promoSlide3 = document.querySelector(".promo-3");
+
+if (promoBtn1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    promoSlide1.classList.add("promo-show");
+    promoSlide1.classList.remove("promo-hidden");
+    promoSlide2.classList.remove("promo-show");
+    promoSlide3.classList.remove("promo-show");
+    promoBtn1.classList.add("current");
+    promoBtn2.classList.remove("current");
+    promoBtn3.classList.remove("current");
+  }));
+
+if (promoBtn2.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    promoSlide2.classList.add("promo-show");
+    promoSlide1.classList.add("promo-hidden");
+    promoSlide3.classList.remove("promo-show");
+    promoBtn1.classList.remove("current");
+    promoBtn2.classList.add("current");
+    promoBtn3.classList.remove("current");
+  }));
+
+if (promoBtn3.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    promoSlide3.classList.add("promo-show");
+    promoSlide1.classList.add("promo-hidden");
+    promoSlide2.classList.remove("promo-show");
+    promoBtn1.classList.remove("current");
+    promoBtn2.classList.remove("current");
+    promoBtn3.classList.add("current");
+  }));
+
+var control1 = document.querySelector(".control-1");
+var control2 = document.querySelector(".control-2");
+var control3 = document.querySelector(".control-3");
+
+var slide1 = document.querySelector(".delivery");
+var slide2 = document.querySelector(".warranty");
+var slide3 = document.querySelector(".credit");
+
+if (control1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide1.classList.add("service-show");
+    slide1.classList.remove("service-hidden");
+    slide2.classList.remove("service-show");
+    slide3.classList.remove("service-show");
+    control1.classList.add("current");
+    control2.classList.remove("current");
+    control3.classList.remove("current");
+  }));
+
+if (control2.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide2.classList.add("service-show");
+    slide1.classList.add("service-hidden");
+    slide3.classList.remove("service-show");
+    control2.classList.add("current");
+    control1.classList.remove("current");
+    control3.classList.remove("current");
+  }));
+
+if (control3.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide3.classList.add("service-show");
+    slide1.classList.add("service-hidden");
+    slide2.classList.remove("service-show");
+    control3.classList.add("current");
+    control1.classList.remove("current");
+    control2.classList.remove("current");
+  }));
